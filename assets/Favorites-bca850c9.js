332488078132b10698a45a842f03faa247990fc8
prokopechkok <104778176+prokopechkok@users.noreply.github.com>
2024-06-12 15:12:35 +0000

    Deploying to gh-pages from @ prokopechkok/project-nanny@635793418b40d71ab04e5604b901528dd04a6f5b 🚀

--- assets/Favorites-bca850c9.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-7cd7672d.js";const e=()=>r.jsx("div",{children:"Favorites Page"});export{e as default};
--- assets/Nannies-06dbb769.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-7cd7672d.js";const s="_page_xf38e_1",a={page:s},t=()=>e.jsx("div",{className:a.page,children:"Nannies Page"});export{t as default};
